428bcb9167d94efa757432e309ed86520c4b8517
Enrico <enrico@giacomazzi.cc>
2022-01-20 15:13:45 +0100

    using fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
+
+  const [pokemon, setPokemon] = useState<Array<any>>([]);
+  const [error, setError] = useState<boolean>(false);
+
+  useEffect(() => {
+
+    fetch('https://pokeapi.co/api/v2/pokemongg')
+      .then(res => res.status < 399 ? res.json() : Promise.reject(res.statusText))
+      // .then(res => {
+      //   if(res.status < 399) {
+      //     return res.json();
+      //   } else {
+      //     return Promise.reject(res.statusText);
+      //   }
+      // })
+      .then(data => {
+        setPokemon(data.results);
+      })
+      .catch(err => {
+        console.error('erroraccio', err);
+        setError(true);
+      })
+
+    // try{
+      
+    // } catch (e) {
+    //   console.log('err 2', e)
+    // }
+    
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      {error && <div>Errore!!!</div>}
+      <ul>
+        {pokemon.map(p => <li key={p.name}>{p.name}</li>)}
+      </ul>
+    </>
+    
+  )
 }
 
 export default App;
